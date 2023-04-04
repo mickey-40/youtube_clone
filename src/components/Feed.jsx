a3f866462ce4c4ef41316a447808335fe0e8407a
mickey-40 <mickeyarnold53@yahoo.com>
2023-04-04 17:49:04 -0500

    commented out unused code.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 import Sidebar from './Sidebar'
 
