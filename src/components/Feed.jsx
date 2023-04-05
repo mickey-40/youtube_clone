28f169f1faa1f534d2bf6a453cea96ecfd6848bb
mickey-40 <mickeyarnold53@yahoo.com>
2023-04-05 07:27:10 -0500

    connected to youtube v3 at rapidapi

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,27 @@
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
+
 import { Sidebar, Videos } from '../components'
 
 
+
 const Feed = () => {
+
+  const [ selectedCategory, setSelectedCategory ] = useState('New');
+
+  useEffect(()=>{
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
+
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row'}}}>
       <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
-        <Sidebar />
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
         <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
           Copyright 2023 Mickey Media
@@ -15,7 +29,7 @@ const Feed = () => {
       </Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
         <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white'}}>
-          New<span style={{ color: '#F31503'}}>videos</span>
+          {selectedCategory}<span style={{ color: '#F31503'}}>videos</span>
         </Typography>
 
         <Videos videos={[]} />
