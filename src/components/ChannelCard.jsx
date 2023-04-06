776b62d813aeb252442ca37b194e3bb741ca14af
mickey-40 <mickeyarnold53@yahoo.com>
2023-04-06 07:35:28 -0500

    finished videocard and working on channel card

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+import { demoProfilePicture } from '../utils/constants'
 
-const ChannelCard = () => {
+const ChannelCard = ({ channelDetail }) => {
+  console.log(channelDetail)
   return (
-    <div>ChannelCard</div>
+    <Box
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+      }}>
+        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+          <CardContent sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff'}}>
+            <CardMedia 
+              image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+              alt={channelDetail?.snippet?.title}
+              sx={{ borderRadius: '50%', height: '180px', width: '180px'}}
+            />
+          </CardContent>
+        </Link>
+      </Box>
   )
 }
 
