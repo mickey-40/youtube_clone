42207735086501ca5bca9f4607bbfa50dd3da454
mickey-40 <mickeyarnold53@yahoo.com>
2023-04-07 15:01:26 -0500

    completed channel page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -25,7 +25,22 @@ const ChannelDetail = () => {
   }, [id])
 
   return (
-    <div>{id}</div>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(110,163,14,1) 35%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+          height: '300px'
+
+        }} />
+          <ChannelCard channelDetail={channelDetail} marginTop="-110px" />        
+      </Box>
+      <Box display='flex' p="2">
+        <Box sx={{ mr: {sm: '100px'}}} />
+          <Videos videos={videos} />
+        
+      </Box>
+    </Box>
   )
 }
 
